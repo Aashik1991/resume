86a653fbc4c6d4273ff130a875dd847cd3d5694c
Ashik <9779540915aashik@gmail.com>
2024-07-10 20:21:55 +0530

    added basename in browserRouter in navigation.js

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -5,7 +5,7 @@ import ROUTES from './Routes';
 
 const Navigation = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             < Routes>
                 < Route path={ROUTES.home.name} element={ROUTES.home.component}></Route>
                 < Route path={ROUTES.projects.name} element={ROUTES.projects.component}></Route>
